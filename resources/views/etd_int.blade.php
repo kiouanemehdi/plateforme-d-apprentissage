2509f63a858c0d9ab1784d78c7582b4900dad2c5
El Mehdi Kiouane <30392394+kiouanemehdi@users.noreply.github.com>
2020-06-01 13:19:20 +0100

    login + te9sim dyal interface etd et prof

--- resources/views/etd_int.blade.php
+++ resources/views/etd_int.blade.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ URL::asset('css/style_etd.css') }}">
     <title>Document</title>
 </head>
 <body>
-    welcome student
+    <div class="grid-container">
+    <div class="header"></div>
+    <div class="reponse"></div>
+    <div class="write_rep"></div>
+    <div class="post"></div>
+    </div>
 </body>
 </html>
--- resources/views/prof_int.blade.php
+++ resources/views/prof_int.blade.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ URL::asset('css/style_prof.css') }}">
     <title>Document</title>
 </head>
 <body>
-    welcome prof
+    <div class="grid-container">
+    <div class="header"></div>
+    <div class="new_post"></div>
+    <div class="post"></div>
+    <div class="reponse"></div>
+    <div class="avg_qst_rep_time"></div>
+    <div class="people_online"></div>
+    </div>
 </body>
 </html>
